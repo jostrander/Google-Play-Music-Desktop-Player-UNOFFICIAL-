24210982e723724d8abac7abd208993a7c64aa14
Jesse Ostrander <jesse.ostrander@gmail.com>
2020-10-17 20:11:54 -0400

    Remove devtron, its not maintained and appears to no longer work

--- src/renderer/generic/core/index.js
+++ src/renderer/generic/core/index.js
@@ -1,15 +1,6 @@
 import { remote } from 'electron';
 
 if (remote.getGlobal('DEV_MODE')) {
-  // Attempt to install DevTron
-  try {
-    if (!remote.BrowserWindow.getDevToolsExtensions().hasOwnProperty('devtron')) {
-      require('devtron').install();
-    }
-  } catch (e) {
-    // Who cares
-  }
-
   window.addEventListener('load', () => {
     const webview = document.querySelector('webview');
     if (!webview) {
